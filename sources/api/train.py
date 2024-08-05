856dec8b025f3f8a19333a5dcc6429a6db585ee9
x0y14 <ya01ma08@gmail.com>
2024-08-05 10:59:06 +0900

    動作確認してるコードを分離

--- sources/api/train.py
+++ sources/api/train.py
@@ -34,8 +34,3 @@ def load_station_data(path: str) -> list[TrainStation]:
 
     return result
 
-
-# if __name__ == "__main__":
-#     r = load_station_data("../../dataset/stations/N02-20_Station.geojson")
-#     print(r[3].geometry)
-#     print(r[3].geometry.Coordinates)
