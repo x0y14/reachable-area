3f1fdcd1cbba701ff64747048094c0ebe1ee9c75
x0y14 <ya01ma08@gmail.com>
2024-10-22 12:40:31 +0900

    isochrone内のstation取得するやつ

--- backend/engine/search.py
+++ backend/engine/search.py
@@ -4,10 +4,10 @@ import geopandas
 from shapely.geometry import Point
 
 
-def search_nearby_stations(
+def search_nearby_walking_distance_stations(
         api: MapBoxApi,
         dataset: dict[TransitType, list[Station]],
-        user_input: Coordinate,
+        coordinate: Coordinate,
         time_limits: list[int],
         transit_types: list[TransitType]
 ) -> dict[int, list[Station]]:
@@ -17,7 +17,7 @@ def search_nearby_stations(
     # MapboxApiを用いてisochroneを取得
     isochrones: dict = api.get_isochrone(
         prof=IsochroneProfile.Walking,
-        coordinate=user_input,
+        coordinate=coordinate,
         contours_minutes=time_limits,
     )
 
@@ -45,5 +45,32 @@ def search_nearby_stations(
             is_include = bool(walking_area.contains(candidate_point)[0])
             if is_include:
                 nearby_stations[time_limit].append(candidate)
+
+    return nearby_stations
+
+
+# search_nearby_walking_distance_stationsのisochroneを既に持ってる場合の版
+def get_stations_contain_area(
+        dataset: dict[TransitType, list[Station]],
+        isochrone: dict,
+        transit_types: list[TransitType]
+) -> list[Station]:
+    nearby_stations: list[Station] = []
+
+    isochrone_gpd = geopandas.GeoDataFrame.from_features(isochrone)
+
+    # チェック対象を確認
+    candidates: list[Station] = []
+    if TransitType.BUS in transit_types:
+        candidates += dataset[TransitType.BUS]
+    if TransitType.TRAIN in transit_types:
+        candidates += dataset[TransitType.TRAIN]
+
+    for candidate in candidates:
+        candidate_coord = candidate.geometry.calc_mean()
+        candidate_point = Point(candidate_coord.Lng, candidate_coord.Lat)
+        is_include = bool(isochrone_gpd.contains(candidate_point)[0])
+        if is_include:
+            nearby_stations.append(candidate)
 
     return nearby_stations
